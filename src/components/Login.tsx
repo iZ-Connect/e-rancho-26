2a14246deeb559321661c967e8277f1da4cc5b62
Ezequiel Fagundes <ezequielfagundes.ef@gmail.com>
2026-01-31 20:13:35 -0300

    Update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -105,7 +105,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
           </form>
 
           <div className="p-4 bg-white/5 text-xs text-slate-500 flex justify-center gap-2">
-            <HelpCircle className="w-4 h-4" /> Contate o S1
+            <HelpCircle className="w-4 h-4" /> Contate o Rancho
           </div>
         </div>
       </div>
